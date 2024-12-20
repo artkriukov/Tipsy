6db1380f5412d6932bb13d9bc411ddd39cd935c6
Artem Kriukov <art.kriukov@gmail.com>
2024-12-20 18:59:54 +0300

    feat: implement tipChanged method for handling tip selection

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -8,21 +8,38 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-
+    var tip = 0.0
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let billTitle = sender.currentTitle!
+        let billTitleWithoutPercentSign = String(billTitle.dropLast())
+        let billAsNumber = Double(billTitleWithoutPercentSign)!
+        tip = billAsNumber / 100
     }
-
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
     }
 }
 
