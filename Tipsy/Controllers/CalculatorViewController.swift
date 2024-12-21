7513d019dd3f088c4aa290fc7f492a907259ea49
Artem Kriukov <art.kriukov@gmail.com>
2024-12-21 14:53:10 +0300

    feat: added handling for the calculate button press and implemented bill calculation logic

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,6 +16,9 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
 
+    
+    var billTotal = 0.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
     }
 
@@ -23,6 +26,13 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let bill = billTextField.text else { return }
+
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+        }
     }
 }
 
